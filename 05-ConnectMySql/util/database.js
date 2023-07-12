48468d51b913729d804cb7de4aa063321e131592
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2023-07-12 15:36:24 +0200

    working with Sequelize, add User model

--- 05-ConnectMySql/util/database.js
+++ 05-ConnectMySql/util/database.js
@@ -1,4 +1,14 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize("node-complete", "root", "Jnaitsabes9", {
+  dialect: "mysql",
+  host: "localhost",
+});
+
+module.exports = sequelize;
+
+// direct connection with mysql2
+/* const mysql = require("mysql2");
 
 const pool = mysql.createPool({
   host: "localhost",
@@ -7,4 +17,4 @@ const pool = mysql.createPool({
   password: "Jnaitsabes9",
 });
 
-module.exports = pool.promise();
+module.exports = pool.promise(); */
